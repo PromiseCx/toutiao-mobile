6520996bc7a45c9f5d2705ea6b9216c2cc46f481
chen <2735569618@qq.com>
2022-07-02 18:18:14 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <!-- 路由的出口 -->
-    <router-view/>
+    <router-view />
+
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 import store from './store'
 
 import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
